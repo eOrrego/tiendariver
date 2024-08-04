cce3ba3728a18ad34138452fb7696a2837a8bc4d
Esteban Orrego <orregoe@gmail.com>
2024-08-04 11:49:19 -0300

    feat: Add remote image patterns for Tienda River

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+            {
+                protocol: 'https',
+                hostname: 'tiendariver.vteximg.com.br',
+            },
+        ],
+    },
+};
 
 export default nextConfig;

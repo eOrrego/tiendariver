ff9d8b695e247864cb2866cd43a302b7addd71fe
Esteban Orrego <orregoe@gmail.com>
2024-08-07 20:52:11 -0300

    feat: Add CategoriesBanner component and update layout in Tienda River

--- src/app/(tiendariver)/(home)/page.tsx
+++ src/app/(tiendariver)/(home)/page.tsx
@@ -1,4 +1,4 @@
-import { InfoCards, PublicBanner } from "@/components";
+import { CategoriesBanner, InfoCards, PublicBanner } from "@/components";
 
 export default function Home() {
 
@@ -18,8 +18,7 @@ export default function Home() {
   return (
     <section>
       <InfoCards />
-      <h2>Tienda River</h2>
-      <p>Tienda de ropa de River Plate</p>
+      <CategoriesBanner />
       <PublicBanner images={images} />
     </section>
   );

640adbceb8668a366c1e1f115fe85b09ab4f7ea7
Esteban Orrego <orregoe@gmail.com>
2024-08-07 16:45:03 -0300

    feat: Add InfoCards component to Tienda River layout

--- src/app/(tiendariver)/(home)/page.tsx
+++ src/app/(tiendariver)/(home)/page.tsx
@@ -1,6 +1,9 @@
+import { InfoCards } from "@/components";
+
 export default function Home() {
   return (
     <section>
+      <InfoCards />
       <h2>Tienda River</h2>
       <p>Tienda de ropa de River Plate</p>
     </section>

31c4004eae6d68f46d4c97b68d03d9898891cbe2
Esteban Orrego <orregoe@gmail.com>
2024-08-07 18:05:23 -0300

    feat: Add lazy loading for images in Tienda River home page

--- src/app/(tiendariver)/(home)/page.tsx
+++ src/app/(tiendariver)/(home)/page.tsx
@@ -1,11 +1,26 @@
-import { InfoCards } from "@/components";
+import { InfoCards, PublicBanner } from "@/components";
 
 export default function Home() {
+
+  const images = [
+    {
+      src: 'https://tiendariver.vteximg.com.br/arquivos/Tercera_Boton.png',
+      alt: 'Tercera Boton',
+      href: '/tercera-boton',
+    },
+    {
+      src: 'https://tiendariver.vteximg.com.br/arquivos/Boton-Termos.png',
+      alt: 'Boton Termos',
+      href: '/boton-termos',
+    },
+  ];
+
   return (
     <section>
       <InfoCards />
       <h2>Tienda River</h2>
       <p>Tienda de ropa de River Plate</p>
+      <PublicBanner images={images} />
     </section>
   );
 }

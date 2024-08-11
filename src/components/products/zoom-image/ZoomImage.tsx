4e594c6c84bfb30dbe5e434baec44322fc277203
Esteban Orrego <orregoe@gmail.com>
2024-08-11 00:30:21 -0300

    feat: Mejorar estilos y agregar lazy loading para imágenes de producto en ZoomImage.tsx

--- src/components/products/zoom-image/ZoomImage.tsx
+++ src/components/products/zoom-image/ZoomImage.tsx
@@ -22,29 +22,30 @@ export const ZoomImage: React.FC<ZoomImageProps> = ({ images }) => {
 
     return (
         <>
-            <div className="flex md:w-1/2">
-                <div className="flex flex-col items-center space-y-2 mr-4">
+            <div className="flex md:w-11/12 ">
+                <div className="flex flex-col items-center space-y-4 mr-4">
                     {images.map((image, index) => (
-                        <div key={`thumbnail-${index}-${image}`} className="relative w-20 h-20 cursor-pointer">
+                        <div key={`thumbnail-${index}-${image}`} className="cursor-pointer">
                             <Image
                                 src={image}
                                 alt={`Thumbnail ${index + 1}`}
-                                width={80}
-                                height={80}
+                                width={150}
+                                height={150}
                                 onClick={() => handleImageSelect(image)}
+                                className="hover:p-1 hover:border hover:border-red-500"
                             />
                         </div>
                     ))}
                 </div>
-                <div className="relative w-full h-96">
+                <div className="relative mx-6">
                     <Image
                         src={selectedImage}
                         alt="Main Product"
-                        fill
-                        className="object-cover"
+                        width={615}
+                        height={615}
                     />
                     <button
-                        className="absolute top-2 left-2 bg-white p-2 rounded-full shadow-md"
+                        className="absolute top-2 left-2 bg-white p-2 rounded-full hover:text-red-500 shadow-md"
                         onClick={handleZoomToggle}
                     >
                         {isZoomed ? <FaTimes size={20} /> : <FaSearchPlus size={20} />}
@@ -53,16 +54,17 @@ export const ZoomImage: React.FC<ZoomImageProps> = ({ images }) => {
             </div>
 
             {isZoomed && (
-                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50 transition-opacity duration-300 ease-in-out">
-                    <div className="relative w-3/4 h-3/4 transform transition-transform duration-300 ease-in-out scale-100">
+                <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-75 z-50">
+                    <div className="scale-110">
                         <Image
                             src={selectedImage}
                             alt="Zoomed Product"
-                            fill
-                            className="object-contain"
+                            width={615}
+                            height={615}
+                            className='m-auto'
                         />
                         <button
-                            className="absolute top-2 right-2 bg-white p-2 rounded-full shadow-md"
+                            className="absolute top-2 right-2 bg-white p-2 rounded-full hover:text-red-500 shadow-md"
                             onClick={handleZoomToggle}
                         >
                             <FaTimes size={24} />

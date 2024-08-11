0ef688a751fe8fb56856d9a5f9b1f40ca8ebbe84
Esteban Orrego <orregoe@gmail.com>
2024-08-10 23:20:13 -0300

    feat: Mejorar estilo de botones y agregar componente Breadcrumb en AddToCart

--- src/components/products/add-to-cart/AddToCart.tsx
+++ src/components/products/add-to-cart/AddToCart.tsx
@@ -15,14 +15,16 @@ export const AddToCart: React.FC<AddToCartProps> = ({ onAddToCart }) => {
 
     return (
         <div>
-            <div className="flex items-center mt-4">
-                <button onClick={() => handleQuantityChange(-1)} className="px-4 py-2 border">-</button>
-                <span className="px-4 py-2 border-t border-b">{quantity}</span>
-                <button onClick={() => handleQuantityChange(1)} className="px-4 py-2 border">+</button>
+            <div className="flex items-center my-4 py-6">
+                <div className='border'>
+                    <button onClick={() => handleQuantityChange(-1)} className="px-4 py-2">-</button>
+                    <span className="px-4 py-2">{quantity}</span>
+                    <button onClick={() => handleQuantityChange(1)} className="px-4 py-2">+</button>
+                </div>
+                <button onClick={() => onAddToCart(quantity)} className=" bg-black text-white ml-8 px-6 py-2">
+                    Agregar al carrito
+                </button>
             </div>
-            <button onClick={() => onAddToCart(quantity)} className="mt-4 bg-black text-white px-4 py-2 rounded">
-                Agregar al carrito
-            </button>
         </div>
     );
 };

cebb155ad8dd3bd288ebb4c8568aaaf66b0253f9
Esteban Orrego <orregoe@gmail.com>
2024-08-11 16:09:54 -0300

    feat: Agregar lazy loading para imágenes de producto en ProductCard

--- src/components/products/product-card/ProductCard.tsx
+++ src/components/products/product-card/ProductCard.tsx
@@ -2,6 +2,7 @@
 
 import React, { useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 // Interfaces props for ProductCard
 interface ProductCardProps {
@@ -22,29 +23,32 @@ export const ProductCard = ({ product }: ProductCardProps) => {
 
     return (
         <div className="p-4">
-            <div
-                className="relative cursor-pointer"
-                onMouseEnter={() => setIsHovered(true)}
-                onMouseLeave={() => setIsHovered(false)}
-            >
-                <Image
-                    src={isHovered ? product.image2 : product.image1}
-                    alt={product.title}
-                    width={500}
-                    height={400}
-                />
-                {product.isNew && (
-                    <div className="absolute top-2 right-2 bg-red-600 text-white text-xs px-2 py-1 rounded">NUEVO INGRESO</div>
-                )}
-            </div>
-            <div className="mt-4">
-                <div className="text-gray-600 text-sm">{product.category}</div>
-                <div className="text-lg font-semibold">{product.title}</div>
-                <div className="text-red-600 text-lg font-bold">${product.price}</div>
-                {product.discountPrice && (
-                    <div className="text-gray-600 line-through text-sm">Precio exclusivo socios ${product.discountPrice}</div>
-                )}
-            </div>
+            {/* Link de prueba para redireccionar a la página de detalle del producto */}
+            <Link href={`/products/asd`}>
+                <div
+                    className="relative cursor-pointer"
+                    onMouseEnter={() => setIsHovered(true)}
+                    onMouseLeave={() => setIsHovered(false)}
+                >
+                    <Image
+                        src={isHovered ? product.image2 : product.image1}
+                        alt={product.title}
+                        width={500}
+                        height={400}
+                    />
+                    {product.isNew && (
+                        <div className="absolute top-2 right-2 bg-red-600 text-white text-xs px-2 py-1 rounded">NUEVO INGRESO</div>
+                    )}
+                </div>
+                <div className="mt-4">
+                    <div className="text-gray-600 text-sm">{product.category}</div>
+                    <div className="text-lg font-semibold">{product.title}</div>
+                    <div className="text-red-600 text-lg font-bold">${product.price}</div>
+                    {product.discountPrice && (
+                        <div className="text-gray-600 line-through text-sm">Precio exclusivo socios ${product.discountPrice}</div>
+                    )}
+                </div>
+            </Link>
         </div>
     );
 };

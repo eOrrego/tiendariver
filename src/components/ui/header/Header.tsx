800e8004ba281340b2ac1b3e8da68f9932f24716
Esteban Orrego <orregoe@gmail.com>
2024-08-12 00:50:12 -0300

    feat: Agregar lazy loading para imágenes de producto en el encabezado

--- src/components/ui/header/Header.tsx
+++ src/components/ui/header/Header.tsx
@@ -1,9 +1,18 @@
+'use client';
+
+import { PopoverCart } from "@/components";
 import Image from "next/image";
 import Link from "next/link";
+import { useState } from "react";
 import { FaUser, FaShoppingCart } from "react-icons/fa";
 
 
 export function Header() {
+    const [isCartOpen, setIsCartOpen] = useState(false);
+
+    const toggleCart = () => {
+        setIsCartOpen(!isCartOpen);
+    };
 
     const logo = "https://tiendariver.vteximg.com.br/arquivos/newLogo_1200x300_rojoynegro.png"
 
@@ -17,8 +26,9 @@ export function Header() {
             </div>
             <div className="flex flex-1 justify-end items-center space-x-4">
                 <FaUser className="text-black text-2xl" />
-                <FaShoppingCart className="text-black text-2xl" />
+                <FaShoppingCart className="text-black text-2xl cursor-pointer" onClick={toggleCart} />
             </div>
+            {isCartOpen && <PopoverCart onClose={toggleCart} />}
         </header>
     );
 }

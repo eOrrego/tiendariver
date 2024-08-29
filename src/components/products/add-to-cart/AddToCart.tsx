71cf0bad45a413268ac2d532159821d477e93b4a
Esteban Orrego <orregoe@gmail.com>
2024-08-28 23:10:03 -0300

    feat: Ajustar componente AddToCart para recibir función desde el padre

--- src/components/products/add-to-cart/AddToCart.tsx
+++ src/components/products/add-to-cart/AddToCart.tsx
@@ -3,7 +3,7 @@
 import React, { useState } from 'react';
 
 interface AddToCartProps {
-    onAddToCart: (quantity: number) => void;
+    onAddToCart: (quantity: number) => void; // Ajuste para recibir la función desde el padre
 }
 
 export const AddToCart: React.FC<AddToCartProps> = ({ onAddToCart }) => {
@@ -13,19 +13,29 @@ export const AddToCart: React.FC<AddToCartProps> = ({ onAddToCart }) => {
         setQuantity((prevQuantity) => Math.max(1, prevQuantity + amount));
     };
 
+    const handleAddToCart = () => {
+        onAddToCart(quantity); // Se llama a la función pasada como prop con la cantidad actual
+    };
+
     return (
         <div>
             <div className="flex items-center my-4 py-6">
-                <div className='border'>
-                    <button onClick={() => handleQuantityChange(-1)} className="px-4 py-2">-</button>
+                <div className="border">
+                    <button onClick={() => handleQuantityChange(-1)} className="px-4 py-2">
+                        -
+                    </button>
                     <span className="px-4 py-2">{quantity}</span>
-                    <button onClick={() => handleQuantityChange(1)} className="px-4 py-2">+</button>
+                    <button onClick={() => handleQuantityChange(1)} className="px-4 py-2">
+                        +
+                    </button>
                 </div>
-                <button onClick={() => onAddToCart(quantity)} className=" bg-black text-white ml-8 px-6 py-2 hover:bg-red-500 transition-colors duration-300 ease-in-out">
+                <button
+                    onClick={handleAddToCart}
+                    className="bg-black text-white ml-8 px-6 py-2 hover:bg-red-500 transition-colors duration-300 ease-in-out"
+                >
                     Agregar al carrito
                 </button>
             </div>
         </div>
     );
 };
-

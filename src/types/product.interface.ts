c35cb182f307a8dd9398cbfc869a4030d9b84dac
Esteban Orrego <orregoe@gmail.com>
2024-09-01 19:35:27 -0300

    feat: Add Product interface for defining product data structure

---
+++ src/types/product.interface.ts
@@ -0,0 +1,21 @@
+
+
+export interface Product {
+    id: string;
+    title: string;
+    price: number;
+    discountPrice: number;
+    images: string[];
+    category: string;
+    subcategory: string;
+    description: string;
+    sizes: {
+        XS: number; // Stock para la talla XS
+        S: number;  // Stock para la talla S
+        M: number;  // Stock para la talla M
+        L: number;  // Stock para la talla L
+        XL: number; // Stock para la talla XL
+        XXL: number; // Stock para la talla 2XL
+    };
+    isNew: boolean;
+}

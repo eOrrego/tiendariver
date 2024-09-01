7a4e1c32f20190f9c80f1f3c246720d7efb13797
Esteban Orrego <orregoe@gmail.com>
2024-09-01 01:20:42 -0300

    feat: Implementar lazy loading para las imágenes de los productos en la página de detalle

--- src/app/(tiendariver)/products/[id]/page.tsx
+++ src/app/(tiendariver)/products/[id]/page.tsx
@@ -1,116 +1,25 @@
 'use client';
 
-import { useEffect, useState } from 'react';
-import {
-    AddToCart,
-    Breadcrumb,
-    FavoriteProduct,
-    PopoverInfo,
-    ProductInfoCards,
-    SelectSizes,
-    ZoomImage,
-} from '@/components';
-import { useCartStore } from '@/store/CartStore';
+import { Suspense } from 'react';
+import { useProductData } from '@/hooks/useProductData';
+import { ProductDetail, ProductSkeleton } from '@/components';
 
-interface Product {
-    id: string;
-    title: string;
-    price: number;
-    discountPrice: number;
-    images: string[];
-    category: string;
-    subcategory: string;
-    description: string;
-}
-
-// Promociones mock, puedes ajustar o eliminar si las obtienes de la API
-const arrayPromotions = {
-    promotions: [
-        {
-            logo: 'https://tiendariver.vteximg.com.br/arquivos/AmericanExpress-river.png',
-            text: '3 cuotas sin interés de $53.333',
-        },
-        {
-            logo: 'https://tiendariver.vteximg.com.br/arquivos/Visa-river.png',
-            text: '3 cuotas sin interés de $53.333',
-        },
-        {
-            logo: 'https://tiendariver.vteximg.com.br/arquivos/Mastercard-river.png',
-            text: '3 cuotas sin interés de $53.333',
-        },
-    ],
-};
 
 const ProductDetailPage = ({ params }: { params: { id: string } }) => {
-    const [product, setProduct] = useState<Product | null>(null);
-    const addToCart = useCartStore((state) => state.addToCart); // Obtén la función addToCart del store
+    const { product, loading, error } = useProductData(params.id);
 
-    useEffect(() => {
-        const fetchProduct = async () => {
-            try {
-                const response = await fetch(`/api/products/${params.id}`);
-                if (!response.ok) {
-                    throw new Error('Failed to fetch product');
-                }
-                const data = await response.json();
-                setProduct(data);
-            } catch (error) {
-                console.error(error);
-            }
-        };
-
-        fetchProduct();
-    }, [params.id]);
-
-    if (!product) {
-        return <p>Loading...</p>;
+    if (error) {
+        return <p>Error al cargar el producto.</p>;
     }
 
-    const breadcrumbPaths = [
-        { label: 'Home', href: '/' },
-        { label: product.category, href: '/products' },
-        { label: product.subcategory, href: '/' },
-    ];
-
-
-    const handleAddToCart = (quantity: number) => {
-        addToCart(
-            {
-                id: product.id,
-                title: product.title,
-                price: product.price,
-                image: product.images[0],
-                quantity,
-            },
-            quantity
-        );
-    };
+    if (loading) {
+        return <ProductSkeleton />;
+    }
 
     return (
-        <div className="container mx-auto p-4">
-            <Breadcrumb paths={breadcrumbPaths} />
-            <div className="flex flex-col md:flex-row">
-                <ZoomImage images={product.images} />
-                <div className="md:px-8 m-auto">
-                    <div className="flex items-center justify-between">
-                        <h1 className="text-3xl font-bold">{product.title}</h1>
-                        <FavoriteProduct productId={product.id} initialFavorite={false} />
-                    </div>
-                    <div className="text-red-600 text-2xl font-bold mt-2">${product.price}</div>
-                    <div className="text-gray-600 text-sm line-through mt-1">
-                        Precio exclusivo socios ${product.discountPrice}
-                    </div>
-                    <PopoverInfo title="¡Mirá nuestra promociones bancarias y formas de pago!" info={arrayPromotions.promotions} />
-                    <SelectSizes onSelectSize={() => { }} />
-                    <AddToCart onAddToCart={handleAddToCart} />
-                    <ProductInfoCards />
-                </div>
-            </div>
-            <div className="mt-8">
-                <h2 className="text-2xl font-bold pb-3 mb-4 border-b-[1px] border-red-500 ">Descripción</h2>
-                <p className="whitespace-pre-line">{product.description}</p>
-            </div>
-        </div>
+        <Suspense fallback={<ProductSkeleton />}>
+            {product && <ProductDetail product={product} />}
+        </Suspense>
     );
 };
 

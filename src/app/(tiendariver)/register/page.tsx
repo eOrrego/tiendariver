beaf551a1d4b7195fc8e6a78ae3193e0a3fcc2a2
Esteban Orrego <orregoe@gmail.com>
2024-09-07 17:59:47 -0300

    feat: Agregar página de registro
    Agrega la página de registro con el componente RegisterForm para manejar el registro de usuarios en formularios. La página muestra un formulario de registro y un enlace para iniciar sesión si ya se tiene una cuenta.

---
+++ src/app/(tiendariver)/register/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import { RegisterForm } from '@/components/ui/form/RegisterForm';
+
+const RegisterPage = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-lg bg-white p-8 rounded shadow-md">
+                <h2 className="text-2xl font-bold mb-4 text-center">Registrarse</h2>
+                <RegisterForm />
+                <p className="mt-4 text-center">
+                    ¿Ya tienes una cuenta?{' '}
+                    <a href="/login" className="text-blue-600 hover:underline">
+                        Iniciar sesión
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default RegisterPage;

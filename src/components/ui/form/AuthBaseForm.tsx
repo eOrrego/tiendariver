2fa14c1003a646474579fbebbbbc299f993e5ce7
Esteban Orrego <orregoe@gmail.com>
2024-09-07 17:59:18 -0300

    feat: Agregar componente AuthBaseForm para manejar la entrada de datos en formularios de autenticación

---
+++ src/components/ui/form/AuthBaseForm.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import React from 'react';
+import { useForm, SubmitHandler } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { FormInput } from './FormInput';
+
+// Tipos comunes del formulario
+interface AuthBaseFormProps<T> {
+    schema: z.ZodType<T>;
+    onSubmit: SubmitHandler<T>;
+    fields: Array<{
+        label: string;
+        type: string;
+        name: keyof T;
+    }>;
+    buttonLabel: string;
+}
+
+export function AuthBaseForm<T>({ schema, onSubmit, fields, buttonLabel }: Readonly<AuthBaseFormProps<T>>) {
+    const {
+        register,
+        handleSubmit,
+        formState: { errors },
+    } = useForm<T>({
+        resolver: zodResolver(schema),
+    });
+
+    return (
+        <form onSubmit={handleSubmit(onSubmit)} className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+            {fields.map((field) => (
+                <FormInput
+                    key={String(field.name)}
+                    label={field.label}
+                    type={field.type}
+                    name={String(field.name)}
+                    register={register}
+                    error={errors[field.name] as any}
+                />
+            ))}
+            <button
+                type="submit"
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+            >
+                {buttonLabel}
+            </button>
+        </form>
+    );
+}

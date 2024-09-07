433140c6b18c5332680c60a7e6ead7163d135fff
Esteban Orrego <orregoe@gmail.com>
2024-09-07 17:59:43 -0300

    feat: Agregar página de inicio de sesión
    Se agregó el archivo `page.tsx` en la carpeta `src/app/(tiendariver)/login` para implementar la página de inicio de sesión. Esta página muestra un formulario de inicio de sesión utilizando el componente `LoginForm` y también incluye un enlace para registrarse en caso de no tener una cuenta.

---
+++ src/app/(tiendariver)/login/page.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import React from 'react';
+import { LoginForm } from '@/components/ui/form/LoginForm';
+
+const LoginPage = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <div className="w-full max-w-md bg-white p-8 rounded shadow-md">
+                <h2 className="text-2xl font-bold mb-4 text-center">Iniciar sesión</h2>
+                <LoginForm />
+                <p className="mt-4 text-center">
+                    ¿No tienes una cuenta?{' '}
+                    <a href="/register" className="text-blue-600 hover:underline">
+                        Regístrate
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+export default LoginPage;

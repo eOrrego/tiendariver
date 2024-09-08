eda08c0c9ee182b91f8d02aaba863f901512a859
Esteban Orrego <orregoe@gmail.com>
2024-09-08 01:10:19 -0300

    refactor: Desestructurar el estado de autenticación en el formulario de registro y en la página de inicio de sesión

--- src/services/userService.ts
+++ src/services/userService.ts
@@ -1,13 +1,13 @@
 import { db } from '@/config/firebaseConfig';
-import { doc, setDoc } from 'firebase/firestore';
+import { doc, setDoc, getDoc } from 'firebase/firestore';
 
 // Define la estructura del usuario que guardaremos en Firestore
-interface UserData {
+export interface UserData {
     firstName: string;
     lastName: string;
     birthDate: string;
     email: string;
-    role: 'user' | 'admin'; // Puedes ajustar los roles según sea necesario
+    role: string;
 }
 
 // Función para guardar los datos del usuario en Firestore
@@ -17,5 +17,21 @@ export const saveUserToFirestore = async (userId: string, userData: UserData) =>
         await setDoc(doc(db, 'users', userId), userData);
     } catch (error) {
         throw new Error('Error al guardar los datos del usuario en Firestore: ' + (error as Error).message);
+    }
+};
+
+// Función para obtener los datos del usuario desde Firestore
+export const fetchUserData = async (userId: string) => {
+    try {
+        const userRef = doc(db, 'users', userId);
+        const userSnap = await getDoc(userRef);
+
+        if (!userSnap.exists()) {
+            throw new Error('User not found');
+        }
+
+        return userSnap.data();
+    } catch (error) {
+        throw new Error('Error al obtener los datos del usuario: ' + (error as Error).message);
     }
 };

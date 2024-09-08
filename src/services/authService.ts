ad59847239f6a07cdd6b31153e13507f377f47f1
Esteban Orrego <orregoe@gmail.com>
2024-09-08 01:10:14 -0300

    refactor: Desestructurar el estado de autenticación en el formulario de registro y en la página de inicio de sesión

--- src/services/authService.ts
+++ src/services/authService.ts
@@ -1,21 +1,40 @@
-import { auth } from '@/config/firebaseConfig';
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged } from 'firebase/auth';
+import { auth, db } from '@/config/firebaseConfig';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { doc, getDoc, setDoc } from 'firebase/firestore';
 
 // Función para registrar un nuevo usuario
-export const registerUser = async (email: string, password: string) => {
+export const registerUser = async (email: string, password: string, userData: any) => {
     try {
         const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
+        const user = userCredential.user;
+
+        // Guardar datos adicionales en Firestore
+        await setDoc(doc(db, 'users', user.uid), {
+            email,
+            ...userData,
+        });
+
+        return { ...user, ...userData };
     } catch (error) {
         throw new Error((error as Error).message);
     }
 };
 
-// Función para iniciar sesión con correo y contraseña
+// Función para iniciar sesión con correo y contraseña y obtener detalles adicionales del usuario
 export const loginUser = async (email: string, password: string) => {
     try {
         const userCredential = await signInWithEmailAndPassword(auth, email, password);
-        return userCredential.user;
+        const user = userCredential.user;
+
+        // Obtener los datos del usuario desde Firestore
+        const userDoc = await getDoc(doc(db, 'users', user.uid));
+
+        if (userDoc.exists()) {
+            const userData = userDoc.data();
+            return { ...user, ...userData }; // Combina los datos del usuario de Firebase con los de Firestore
+        } else {
+            throw new Error('Usuario no encontrado en la base de datos.');
+        }
     } catch (error) {
         throw new Error((error as Error).message);
     }
@@ -33,12 +52,13 @@ export const logoutUser = async () => {
 // Función para obtener el usuario actual
 export const getCurrentUser = () => {
     return new Promise((resolve, reject) => {
-        onAuthStateChanged(auth, (user) => {
+        const unsubscribe = auth.onAuthStateChanged((user) => {
             if (user) {
                 resolve(user);
             } else {
-                reject(new Error('No user logged in'));
+                reject(new Error('No hay usuario autenticado'));
             }
+            unsubscribe();
         });
     });
 };

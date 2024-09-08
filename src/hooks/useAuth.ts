facd132af5e954eb8e70c360f7949548ee402697
Esteban Orrego <orregoe@gmail.com>
2024-09-08 01:10:10 -0300

    refactor: Desestructurar el estado de autenticación en el formulario de registro y en la página de inicio de sesión feat: Agregar función para guardar los datos del usuario en Firestore y redirigir después del registro exitoso feat: Agregar función para guardar los datos del usuario en Firestore

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,6 +1,8 @@
 import { useEffect } from 'react';
 import { useAuthStore } from '@/store/authStore';
 import { getCurrentUser } from '@/services/authService';
+import { doc, getDoc } from 'firebase/firestore';
+import { db } from '@/config/firebaseConfig';
 
 export const useAuth = () => {
     const { setAuthenticated, setUser, setRole } = useAuthStore();
@@ -8,13 +10,29 @@ export const useAuth = () => {
     useEffect(() => {
         const fetchUser = async () => {
             try {
-                const user: any = await getCurrentUser();
-                setAuthenticated(true);
-                setUser(user);
-                // Aquí implementar la lógica para determinar el rol del usuario
-                const isAdmin = user.email === 'admin@mail.com';
-                setRole(isAdmin ? 'admin' : 'user');
-            } catch {
+                const user = await getCurrentUser();
+
+                if (user) {
+                    // Obtener datos adicionales del usuario desde Firestore
+                    const userDoc = await getDoc(doc(db, 'users', user.uid));
+                    if (userDoc.exists()) {
+                        const userData = userDoc.data();
+                        setAuthenticated(true);
+                        setUser({ ...user, ...userData }); // Combina los datos de autenticación y Firestore
+                        setRole(userData.role || 'user'); // Configura el rol basado en la información obtenida
+                    } else {
+                        // Si no se encuentran los datos adicionales, maneja el error según sea necesario
+                        setAuthenticated(false);
+                        setUser(null);
+                        setRole('guest');
+                    }
+                } else {
+                    setAuthenticated(false);
+                    setUser(null);
+                    setRole('guest');
+                }
+            } catch (error) {
+                console.error('Error fetching user:', error);
                 setAuthenticated(false);
                 setUser(null);
                 setRole('guest');

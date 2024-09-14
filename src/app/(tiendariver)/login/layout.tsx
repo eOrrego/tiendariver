498d40d2798b8cdca43483e04cc5f98f0dff2b22
Esteban Orrego <orregoe@gmail.com>
2024-09-14 18:55:22 -0300

    Refactoriza el componente de registro para agregar un nuevo layout de inicio de sesión.

---
+++ src/app/(tiendariver)/login/layout.tsx
@@ -0,0 +1,40 @@
+// src/app/(tiendariver)/login/layout.tsx
+'use client';
+
+import { ReactNode, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/store/authStore';
+import Loading from './loading';
+
+interface AuthLayoutProps {
+    children: ReactNode;
+}
+
+const LoginLayout: React.FC<AuthLayoutProps> = ({ children }) => {
+    const { isAuthenticated, isLoading, checkUserSession } = useAuthStore((state) => ({
+        isAuthenticated: state.isAuthenticated,
+        isLoading: state.isLoading,
+        checkUserSession: state.checkUserSession,
+    }));
+    const router = useRouter();
+
+    useEffect(() => {
+        checkUserSession();
+    }, [checkUserSession]);
+
+    useEffect(() => {
+        if (!isLoading && isAuthenticated) {
+            router.push('/profile'); // Redirige al perfil si el usuario está autenticado
+        }
+    }, [isAuthenticated, isLoading, router]);
+
+    // Muestra un loading state si está verificando la sesión
+    if (isLoading) return <Loading />;
+
+    // No renderiza los hijos si el usuario está autenticado
+    if (isAuthenticated) return null;
+
+    return <>{children}</>;
+};
+
+export default LoginLayout;

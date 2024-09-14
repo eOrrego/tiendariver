dfccef39b981f789899994980115fca0417ec5b4
Esteban Orrego <orregoe@gmail.com>
2024-09-14 18:55:14 -0300

    Refactoriza el componente de registro para eliminar código innecesario y mejorar la legibilidad.

--- src/app/(tiendariver)/login/page.tsx
+++ src/app/(tiendariver)/login/page.tsx
@@ -1,20 +1,6 @@
-'use client';
-
-import React, { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
-import { useAuthStore } from '@/store/authStore';
 import { LoginForm } from '@/components/ui/form/LoginForm';
 
 const LoginPage = () => {
-    const { isAuthenticated } = useAuthStore((state) => ({ isAuthenticated: state.isAuthenticated }));
-    const router = useRouter();
-
-    useEffect(() => {
-        if (isAuthenticated) {
-            router.push('/'); // Redirige si el usuario ya está autenticado
-        }
-    }, [isAuthenticated, router]);
-
     return (
         <div className="flex items-center justify-center min-h-screen bg-gray-100">
             <div className="w-full max-w-md bg-white p-8 rounded shadow-md">

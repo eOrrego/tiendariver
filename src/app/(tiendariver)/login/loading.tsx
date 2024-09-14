f9d071a15a57c91f5d901f13b7bd789278bc919c
Esteban Orrego <orregoe@gmail.com>
2024-09-14 18:55:18 -0300

    Refactoriza el componente de registro para eliminar código innecesario y mejorar la legibilidad.

---
+++ src/app/(tiendariver)/login/loading.tsx
@@ -0,0 +1,10 @@
+
+const Loading = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <p className="text-xl font-semibold">Cargando...</p>
+        </div>
+    );
+};
+
+export default Loading;

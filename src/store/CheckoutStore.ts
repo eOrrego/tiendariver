6fb58bf27ca3aa0c641a63950a6b941822281b46
Esteban Orrego <orregoe@gmail.com>
2024-09-14 21:52:33 -0300

    Agrega lógica para calcular el total de la orden y guardar los productos del carrito en Firestore

--- src/store/CheckoutStore.ts
+++ src/store/CheckoutStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { useCartStore } from './CartStore'; // Importa el store del carrito para acceder a sus datos
 
 interface PersonalData {
     email: string;
@@ -21,13 +22,24 @@ interface PaymentData {
     securityCode: string;
 }
 
+interface CartProduct {
+    id: string;
+    title: string;
+    size: string;
+    price: number;
+    quantity: number;
+    subtotal: number; // Calculamos subtotal por producto
+}
+
 interface CheckoutState {
     personalData: PersonalData;
     deliveryData: DeliveryData;
     paymentData: PaymentData;
+    cartProducts: CartProduct[];
     setPersonalData: (data: PersonalData) => void;
     setDeliveryData: (data: DeliveryData) => void;
     setPaymentData: (data: PaymentData) => void;
+    setCartProducts: (products: CartProduct[]) => void;
     clearCheckoutData: () => void;
 }
 
@@ -50,27 +62,31 @@ export const useCheckoutStore = create<CheckoutState>((set) => ({
         expiryDate: '',
         securityCode: '',
     },
+    cartProducts: [], // Añadimos la lista de productos del carrito
     setPersonalData: (data) => set({ personalData: data }),
     setDeliveryData: (data) => set({ deliveryData: data }),
     setPaymentData: (data) => set({ paymentData: data }),
-    clearCheckoutData: () => set({
-        personalData: {
-            email: '',
-            firstName: '',
-            lastName: '',
-            dni: '',
-            phone: '',
-        },
-        deliveryData: {
-            postalCode: '',
-            deliveryMethod: '',
-            address: '',
-        },
-        paymentData: {
-            cardNumber: '',
-            cardHolder: '',
-            expiryDate: '',
-            securityCode: '',
-        },
-    }),
+    setCartProducts: (products) => set({ cartProducts: products }),
+    clearCheckoutData: () =>
+        set({
+            personalData: {
+                email: '',
+                firstName: '',
+                lastName: '',
+                dni: '',
+                phone: '',
+            },
+            deliveryData: {
+                postalCode: '',
+                deliveryMethod: '',
+                address: '',
+            },
+            paymentData: {
+                cardNumber: '',
+                cardHolder: '',
+                expiryDate: '',
+                securityCode: '',
+            },
+            cartProducts: [],
+        }),
 }));

a64b963514f81f7e4dd1aea0b3206d71b0e28443
Esteban Orrego <orregoe@gmail.com>
2024-09-14 21:33:24 -0300

    Agrega el componente de carga de perfil

---
+++ src/app/(tiendariver)/checkout/loading.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+import React from 'react';
+
+const CheckoutLoading = () => {
+    return (
+        <div className="flex items-center justify-center min-h-screen bg-gray-100">
+            <p className="text-gray-500">Cargando Checkout...</p>
+        </div>
+    );
+};
+
+export default CheckoutLoading;

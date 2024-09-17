91e386238706a1a39cef7618f501becf039e2316
Esteban Orrego <orregoe@gmail.com>
2024-09-16 23:21:50 -0300

    Refactoriza enlace a la página de órdenes en la barra lateral del perfil

--- src/services/orderService.ts
+++ src/services/orderService.ts
@@ -1,46 +1,46 @@
 // src/services/orderService.ts
-import { addDoc, collection } from 'firebase/firestore';
+import { addDoc, collection, getDocs, query, where } from 'firebase/firestore';
 import { db } from '@/config/firebaseConfig';
-
-interface OrderData {
-    personalData: {
-        firstName: string;
-        lastName: string;
-        email: string;
-        dni: string;
-        phone: string;
-    };
-    deliveryData: {
-        postalCode: string;
-        deliveryMethod: string;
-        address: string;
-    };
-    paymentData: {
-        cardNumber: string;
-        cardHolder: string;
-        expiryDate: string;
-        securityCode: string;
-    };
-    cartProducts: {
-        id: string;
-        title: string;
-        price: number;
-        quantity: number;
-        size: string;
-    }[];
-    userId: string; // Añadir ID del usuario para relacionar la orden con el usuario logueado
-}
+import { OrderData } from '@/types/Order';
 
 // Función para crear una orden en Firestore
 export const createOrder = async (orderData: OrderData) => {
     try {
         const orderRef = await addDoc(collection(db, 'orders'), {
             ...orderData,
-            createdAt: new Date(),
+            createdAt: new Date(), // Agrega la fecha de creación
         });
 
         return orderRef.id; // Devuelve el ID de la orden creada
     } catch (error) {
         throw new Error('Error al guardar la orden: ' + (error as Error).message);
+    }
+};
+
+// Función para obtener todas las órdenes de un usuario por su userId
+export const fetchUserOrders = async (userEmail: string): Promise<OrderData[]> => {
+    try {
+        const ordersRef = collection(db, 'orders');
+        const q = query(ordersRef, where('userEmail', '==', userEmail));
+        const querySnapshot = await getDocs(q);
+
+        // Mapea los documentos obtenidos a un arreglo de órdenes
+        const orders: OrderData[] = querySnapshot.docs.map((doc) => {
+            const data = doc.data();
+            return {
+                id: doc.id,
+                createdAt: data.createdAt.toDate(), // Convierte el timestamp a Date
+                personalData: data.personalData,
+                deliveryData: data.deliveryData,
+                paymentData: data.paymentData,
+                cartProducts: data.cartProducts,
+                userId: data.userId,
+                userEmail: data.userEmail,
+            } as OrderData;
+        });
+
+        return orders;
+    } catch (error) {
+        throw new Error('Error al obtener las órdenes del usuario: ' + (error as Error).message);
     }
 };
